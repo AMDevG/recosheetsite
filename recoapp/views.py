ab2bd319adfdc7afc01ee7e06019df62c1091150
John Berry <John Berry>
2016-08-22 19:37:19 -0500

    Need to fix upload bug

--- recoapp/views.py
+++ recoapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 
 def reco(request):
 	return render(request, 'index.html')
@@ -18,14 +18,31 @@ def success(request):
         due_date = request.POST.get('dueDate', None)
         message_text = request.POST.get('message', None)
 
-        print("processed request")
-
-        email_reco = "Transaction Name: " + str(trans_name) + '\n' + "Delivery Type: " + str(delivery_type) + '\n' + "City: " + str(city_name) + '\n' + "State: " + str(state_name) + '\n' + "Care Type: " + str(care_type) + '\n' + "Origination Lead: " + str(orig_lead)  + '\n' + "Expected Delivery: " + str(due_date) + '\n' + "Transaction Notes: " + str(message_text)
 
 
+        subject = "New Reco Sheet - " + str(trans_name) 
 
-        send_mail('New Reco Sheet - ' + trans_name, email_reco,"bprecosheet@gmail.com",
-        ['jeberry308@gmail.com'], fail_silently=False)
+        email_text = "Transaction Name: " + str(trans_name) + '\n' + "Delivery Type: " + str(delivery_type) + '\n' + "City: " + str(city_name) + '\n' + "State: " + str(state_name) + '\n' + "Care Type: " + str(care_type) + '\n' + "Origination Lead: " + str(orig_lead)  + '\n' + "Expected Delivery: " + str(due_date) + '\n' + "Transaction Notes: " + str(message_text)
+
+        email_reco = EmailMessage(subject, email_text, "bprecosheet@gmail.com", ['jeberry308@gmail.com'])
+
+        # for f in request.FILES.getlist('due_dil'):
+        #     print("got: ", f)
+        #     email_reco.attach(f, f.read(), f.content_type)
+
+        # for filename, file in request.FILES.iteritems():
+        #     name = request.FILES[filename].name
+
+        #     print(name)
+
+        attach = request.FILES['due_dil']
+
+        email_reco.attach(attach.name, attach.read(), attach.content_type)
+    
+        email_reco.send()
+
+       # send_mail('New Reco Sheet - ' + trans_name, email_reco,"bprecosheet@gmail.com",
+       # ['jeberry308@gmail.com'], fail_silently=False)
 
 
         return render(request, 'success.html')
